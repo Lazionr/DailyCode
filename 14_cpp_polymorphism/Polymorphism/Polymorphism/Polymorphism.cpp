6ecaa1e6d9c0f70c2e5f91cc7bfb308a114ee256
lzh65038-web <lzh65038@gmail.com>
2026-08-26 22:17:36 +0800

    多态
    多态

---
+++ 14_cpp_polymorphism/Polymorphism/Polymorphism/Polymorphism.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+class Person
+{
+public:
+	virtual void BuyTicket() { cout << "买票->全价" << endl; }
+};
+
+class Student : public Person
+{
+public:
+	virtual void BuyTicket() { cout << "买票->半价" << endl; }
+};
+
+void Func(Person* ptr)
+{
+	ptr->BuyTicket();
+}
+
+void test1()
+{
+	Person ps;
+	Student st;
+
+	//多态
+	//派生类指针可以隐式类型转换成基类指针
+	Func(&ps);
+	Func(&st);
+
+}
+
+
+
+int main()
+{
+	test1();
+	return 0;
+}
