085a50c832af7a63ddecfe722e9aa7dd81a9952e
lzh65038-web <lzh65038@gmail.com>
2026-09-01 23:03:08 +0800

    二叉搜索树
    二叉搜索树

---
+++ 15_cpp_binary_search_tree/BinarySearchTree/BinarySearchTree/BinarySearchTree.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<class K>
+struct BSTNode
+{
+	BSTNode(const K& key)
+		:_left(nullptr)
+		,_right(nullptr)
+		,_key(key)
+	{ }
+
+	BSTNode<K>* _left;
+	BSTNode<K>* _right;
+	K _key;
+};
+
+template<class K>
+class BST
+{
+public:
+	using Node = BSTNode<K>;
+private:
+			
+};
+
+int main()
+{
+
+	return 0;
+}
